03d4cd2a068e1a60bab6e096a2105f884a77715b
aluan_fooshe <audreyluan8899@gmail.com>
2026-02-02 10:53:29 -0800

    added python state machine library

--- src/basic_cart_statemachine.py
+++ src/basic_cart_statemachine.py
@@ -1,10 +1,11 @@
 from gpiozero import DistanceSensor  # Import the DistanceSensor class from the gpiozero library
-
 import tkinter as tk  # Import the tkinter library for creating the GUI
-
 from tkinter import font  # Import the font module from tkinter for customizing the font
 
 from time import sleep  # Import the sleep function from the time module for delay
+
+import statemachine # Import the python state machine in order to build a framework that responds to sensor inputs.
+from statemachine import StateMachine, State
 
 """
 GPIO pin 23 (Trig), GPIO pin 24 (Echo), GND and 5V pins from raspberry pi 5
@@ -21,18 +22,12 @@ sensor = DistanceSensor(echo=24, trigger=23, max_distance=5)
 window = tk.Tk()
 
 window.title("Distance Measurement")
-
 custom_font = font.Font(size=30)  # Create a custom font object with size 30
-
 window.geometry("800x400")  # Set the dimensions of the window
-
 distance_label = tk.Label(window, text="Distance: ", anchor='center', font=custom_font)
 
 # Create a label to display the distance, centered text, and use the custom font
-
 distance_label.pack()  # Add the label to the window
-
-array1 = [1, 2, 3, 2, 2, 1, 1, 1, 1, 3, 3, 3]
 
 
 def measure_distance():
@@ -68,31 +63,5 @@ window.mainloop()
 
 # -----------------------------------------------
 
-def switch_case(a):
-    match a:
-        case 1:
-            return "state 1"
-        case 2:
-            return "state 2"
-        case 3:
-            return "state 3"
-        case _:
-            return "state unknown"
 
-def cart_statemachine(a):
-    match a:
-        case "idle":
-            return "idle state"
-        case "follow":
-            return "state 2"
-        case "stuck":
-            return "state 3"
-        case _:
-            return "state unknown"
 
-# Sort the list in place
-array1.sort()
-
-# #  test if switch case actually works
-# for a in array1:
-#     print(switch_case(a))
