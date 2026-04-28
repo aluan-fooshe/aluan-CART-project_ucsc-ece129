b77010583a13f1b2803eb1d343ffea5dc6260334
aluan <aluan@ucsc.edu>
2026-04-28 12:09:14 -0700

    PID Control implemented

--- src_arduino/CART_implementation_arduino/CART_implementation_arduino.ino
+++ src_arduino/CART_implementation_arduino/CART_implementation_arduino.ino
@@ -1,36 +1,95 @@
 #include <SoftwareSerial.h>
 #include "RoboClaw.h"
 
-SoftwareSerial serial(11, 10); // RX, TX
+SoftwareSerial serial(10, 11); // RX, TX
 RoboClaw roboclaw(&serial, 10000);
 
-#define RC_ADDRESS 0x80
+#define ROBOCLAW_ADDRESS 0x80
 #define BAUDRATE 38400
 
+//Velocity PID coefficients.
+#define Kp 1.0
+#define Ki 0.5
+#define Kd 0.25
+#define qpps 44000
+
 void setup() {
-  Serial.begin(BAUDRATE);  // USB serial for debugging
-  serial.begin(BAUDRATE);
-  roboclaw.begin(BAUDRATE);
-  Serial.println("RoboClaw sketch started");
+  //Open Serial and roboclaw serial ports
+  Serial.begin(57600);
+  roboclaw.begin(38400);
+  
+  //Set PID Coefficients
+  roboclaw.SetM1VelocityPID(ROBOCLAW_ADDRESS,Kd,Kp,Ki,qpps);
+  roboclaw.SetM2VelocityPID(ROBOCLAW_ADDRESS,Kd,Kp,Ki,qpps);  
 }
 
 void loop() {
-  int speed = 30;
-
-  for (int i = 0; i <= speed; i++) {
-    moveForward(RC_ADDRESS, i);
-    delay(50);  // controls how fast it speeds up
+  //Set initial speed
+  uint8_t speed = 1000;
+  roboclaw.SpeedM1(ROBOCLAW_ADDRESS, speed);
+  
+  for(uint8_t i = 0;i<1000;i++){
+    roboclaw.SpeedM1(ROBOCLAW_ADDRESS, i);
+    displayspeed();
+    delay(100);
   }
 
-  // Move forward
-  moveForward(RC_ADDRESS, speed);
-  delay(2000);
+  roboclaw.SpeedM1(ROBOCLAW_ADDRESS, speed);
+  delay(1000);
 
-
-  for (int i = speed; i >= 0; i--) {
-    moveForward(RC_ADDRESS, i);
-    delay(50);  // controls how fast it slows down
+  for(uint8_t i = 1000;i>0;i--){
+    roboclaw.SpeedM1(ROBOCLAW_ADDRESS, i);
+    displayspeed();
+    delay(100);
   }
+}
+
+void displayspeed(void)
+{
+  uint8_t status1,status2,status3,status4;
+  bool valid1,valid2,valid3,valid4;
+  
+  int32_t enc1= roboclaw.ReadEncM1(ROBOCLAW_ADDRESS, &status1, &valid1);
+  int32_t enc2 = roboclaw.ReadEncM2(ROBOCLAW_ADDRESS, &status2, &valid2);
+  int32_t speed1 = roboclaw.ReadSpeedM1(ROBOCLAW_ADDRESS, &status3, &valid3);
+  int32_t speed2 = roboclaw.ReadSpeedM2(ROBOCLAW_ADDRESS, &status4, &valid4);
+  Serial.print("Encoder1:");
+  if(valid1){
+    Serial.print(enc1);
+    Serial.print(" ");
+    Serial.print(status1);
+    Serial.print(" ");
+  }
+  else{
+    Serial.print("invalid ");
+  }
+  Serial.print("Encoder2:");
+  if(valid2){
+    Serial.print(enc2);
+    Serial.print(" ");
+    Serial.print(status2);
+    Serial.print(" ");
+  }
+  else{
+    Serial.print("invalid ");
+  }
+  Serial.print("Speed1:");
+  if(valid3){
+    Serial.print(speed1);
+    Serial.print(" ");
+  }
+  else{
+    Serial.print("invalid ");
+  }
+  Serial.print("Speed2:");
+  if(valid4){
+    Serial.print(speed2);
+    Serial.print(" ");
+  }
+  else{
+    Serial.print("invalid ");
+  }
+  Serial.println();
 }
 
 void moveForward(uint8_t address, uint8_t target_speed){
